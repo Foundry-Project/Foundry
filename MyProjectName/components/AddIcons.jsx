dfc3e38d41d517b8ba5f1092c5f27fe783d787ff
fourat anderi <fouratanderi98@gmail.com>
2024-08-21 14:14:57 +0100

    design almost done

--- MyProjectName/components/AddIcons.jsx
+++ MyProjectName/components/AddIcons.jsx
@@ -3,11 +3,11 @@ import { SafeAreaView,Text } from 'react-native'
 import Icons  from 'react-native-vector-icons/Octicons'
 
 
-function AddIcons() {
+function AddIcons({color='white'}) {
   return (
     <SafeAreaView style={{alignItems:'center',justifyContent:'center'}}>
-    <Icons name='diff-added' size={20} color={'white'}></Icons>
-     <Text style={{marginTop:'1%',color:'white'}}>Add</Text>
+    <Icons name='diff-added' size={20} color={color}></Icons>
+     <Text style={{marginTop:'1%',color:color}}>Add</Text>
     </SafeAreaView>  
     )
 }
