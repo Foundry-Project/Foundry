151ec99739246fcb39a0106c708fb0dacab08296
Iheb Laabidi <ihebklaabidi123@gmail.com>
2024-08-22 09:25:16 +0100

    onboarding , leaderboard ,settings 100%

--- MyProjectName/App.js
+++ MyProjectName/App.js
@@ -2,13 +2,20 @@
 // import { StatusBar } from 'expo-status-bar';
 import FirstPage from './Pages/FirstPage';
 import { SafeAreaView } from 'react-native';
-
+import Getback from './Onboarding/Getback';
+import Found from './Onboarding/Found';
+import Welcome from './Onboarding/Welcome';
+import Lost from './Onboarding/Lost';
+import Leaderboard from './Pages/Leaderboard';
+import Settings from './Pages/Settings';
+import NavBar from './components/NavBar'
 export default function App() {
   return (
     <SafeAreaView style={{flex:1}}>
-    <FirstPage/>
+    <Settings/>
+<NavBar/>
     </SafeAreaView>
     
 
 
-
+  )}
