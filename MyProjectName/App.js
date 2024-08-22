988abf740efae10968832dd357835b512270d2fc
amirakaroui1 <amyrakaroui@gmail.com>
2024-08-22 09:32:10 +0100

    save

--- MyProjectName/App.js
+++ MyProjectName/App.js
@@ -1,19 +1,16 @@
 
 // import { StatusBar } from 'expo-status-bar';
 // import FirstPage from './Pages/FirstPage';
-import { SafeAreaView } from 'react-native';
-import AppNavigator from './Pages/AppNavigator';
+// import { SafeAreaView } from 'react-native';
+// import AppNavigator from './Pages/AppNavigator';
 // import Categorycard from './components/Categorycard';
 // import CategoriesList from './components/CategoriesList';
+import Chat from "./Pages/Chat.jsx"
 export default function App() {
   return (
-    <SafeAreaView style={{flex:1}}>
-      <AppNavigator />
-    {/* <FirstPage/> */}
- {/* <AddFromHome /> */}
- {/* <CategoriesList /> */}
-    </SafeAreaView>
-    
 
+    <Chat/>
+  
 
-  )}
+  )
+}
